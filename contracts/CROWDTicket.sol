389e0761a248e81493b78bfb800d609d97a1cf50
phillip <philosup@numbers21.com>
2021-11-16 14:08:31 +0900

    ticket deposit/withdraw

--- contracts/CROWDTicket.sol
+++ contracts/CROWDTicket.sol
@@ -2,13 +2,12 @@
 
 pragma solidity ^0.8.0;
 
-// import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
-// import "@openzeppelin/contracts/security/Pausable.sol";
-// import "@openzeppelin/contracts/access/Ownable.sol";
-
 import "./ICROWDToken.sol";
+import "./ECDSA.sol";
+import "./CROWDValidator.sol";
 
-contract CROWDTicket is ICROWDToken{
+contract CROWDTicket is ICROWDToken, CROWDValidator{
+    using ECDSA for bytes32;
 
     constructor(uint256 amount) ERC20("CROWD.com", "Ticket"){
         _mint(_msgSender(), amount*10**decimals());
@@ -19,9 +18,18 @@ contract CROWDTicket is ICROWDToken{
         revert();
     }
 
-    function claim(address account, uint256 amount, address fee_account, uint256 fee_amount) public onlyOwner{
-        transfer(account, amount);
-        if(fee_account != _msgSender())
-            transfer(fee_account, fee_amount);
+    function deposit(address account, uint256 amount) public{
+        transferFrom(msg.sender, account, amount);
     }
+
+    function withdraw(address account, uint256 amount, uint256 id, bytes memory signature) public{
+        bytes32 _hash = ECDSA.getHash("ticketclaim",id,msg.sender,amount).toEthSignedMessageHash();
+  
+        address signer = _hash.recover(signature);
+        
+        require(signer == getValidator(address(this)), "invalid signer");
+
+        transfer(account, amount);
+    }
+
 }
