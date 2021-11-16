638c6df499941b830c9ea9cc74314b9170afa130
phillip <philosup@numbers21.com>
2021-11-16 14:09:04 +0900

    remove comments

--- contracts/CROWDToken.sol
+++ contracts/CROWDToken.sol
@@ -2,26 +2,12 @@
 
 pragma solidity ^0.8.0;
 
-// import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
-// // import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Pausable.sol";
-// import "@openzeppelin/contracts/security/Pausable.sol";
-// import "@openzeppelin/contracts/access/Ownable.sol";
-
 import "./ICROWDToken.sol";
 
 contract CROWDToken is ICROWDToken{
 
-    // constructor(
-    //     string memory name,
-    //     string memory symbol,
-    //     uint256 initialSupply,
-    //     address owner
-    // ) ERC20(name, symbol) {
-    //     _mint(owner, initialSupply);
-    // }
     constructor(uint256 amount) ERC20("CROWD.com", "CROWD") {
         _mint(_msgSender(), amount*10**decimals());
-        // minimum_swap_amount = 10000*10**decimals();        
     }
 
     //Don't accept ETH or BNB
